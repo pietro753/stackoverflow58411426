efdd300243c697d67faefe5ad3ce7c5431251a71
Peter Csikos <pcsikos@sygic.com>
2019-11-04 12:52:20 +0100

    Initial

---
+++ ClientDisconnectedDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ClientDisconnectedDemo.Controllers
+{
+    [RoutePrefix("api/foo")]
+    public class HomeController : ApiController
+    {
+        //[Route("")]
+        //[HttpGet]
+        //public IHttpActionResult Index()
+        //{
+        //    return Ok();
+        //}
+
+        [Route("bar")]
+        [HttpPut]
+        public async Task<IHttpActionResult> Put()
+        {
+            await Task.Delay(200);
+            return Ok();
+        }
+
+        [Route("test")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetMethod(CancellationToken cancellationToken)
+        {
+            await Task.Delay(100, cancellationToken);
+            return Ok("value");
+        }
+    }
+}
