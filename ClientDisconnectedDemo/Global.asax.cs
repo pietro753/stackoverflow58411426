261fe6a12e60a8ce4ddb3f153e8a019d7d527eaf
Peter Csikos <pcsikos@sygic.com>
2019-11-04 13:04:05 +0100

    Application_EndRequest logging

--- ClientDisconnectedDemo/Global.asax.cs
+++ ClientDisconnectedDemo/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -12,6 +13,12 @@ namespace ClientDisconnectedDemo
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+            Debug.WriteLine($"{context.Response.StatusCode}:{context.Response.SubStatusCode}:{context.Request.Path}:{context.Request.Url.AbsoluteUri}");
         }
     }
 }
